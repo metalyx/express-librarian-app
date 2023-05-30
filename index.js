8ad57bf7ccfe3ac632024a3deedc060b547d8e35
Vitalii <metalyx3@gmail.com>
2023-05-30 13:34:07 -0700

    dotenv and mongo keys

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const authRouter = require('./routers/authRouter');
 const booksRouter = require('./routers/booksRouter');
 const adminRouter = require('./routers/adminRouter');
-const { dbLogin, dbPassword } = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const rootAPIPath = '/api';
@@ -18,7 +19,7 @@ app.use(`${rootAPIPath}/books`, booksRouter);
 const start = async () => {
     try {
         await mongoose.connect(
-            `mongodb+srv://${dbLogin}:${dbPassword}@cluster0.4odlt8i.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@cluster0.4odlt8i.mongodb.net/?retryWrites=true&w=majority`
         );
         app.listen(PORT, () => console.log(`server started, port ${PORT}`));
     } catch (e) {
